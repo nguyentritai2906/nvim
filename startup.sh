01b88adb1c400c91417b83576a2152b011aa4b54
Nguyen Tri Tai <nguyentritai2906@gmail.com>
2023-01-17 20:24:08 +0700

    feat(startup): instruction to install

--- startup.sh
+++ startup.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
+# Check if Neovim is installed
+is_installed=$(command -v nvim 2>&1 >/dev/null; echo $?) # 0 if installed, 1 if not
+
 # Check if neovim is >= 0.8
-if [ "$(nvim --version | head -n 1 | cut -d ' ' -f 2 | cut -d '.' -f 2)" -lt 8 ]; then
+if [[ $is_installed -eq 0 ]]; then
+    nvim_version=$(nvim --version | head -n 1 | cut -d ' ' -f 2)
+    nvim_major=$(echo $nvim_version | cut -d '.' -f 1)
+    nvim_minor=$(echo $nvim_version | cut -d '.' -f 2)
+    if [[ $nvim_major -lt 0 || $nvim_minor -lt 8 ]]; then
+        is_installed=1
+    fi
+fi
+
+if [[ $is_installed -eq 1 ]]; then
     echo "Neovim >= 0.8 is required"
+
+    # echo EOF
+    echo "You can install it with:"
+    echo "  sudo add-apt-repository ppa:neovim-ppa/unstable"
+    echo "  sudo apt update"
+    echo "  sudo apt install neovim"
+
+    echo "Or"
+    echo "  sudo snap install nvim"
+
     exit 1
 fi
 
@@ -10,10 +32,9 @@ fi
 if ! command -v npm &> /dev/null
 then
     echo "npm could not be found"
+
+    echo "You can install it with:"
+    echo "  sudo apt install npm"
+
     exit
 fi
-#
-# Install Packer Nvim
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.Nvim
-
