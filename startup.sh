fdf6e04f08076030bfbab79fcb563164099c9cca
Nguyen Tri Tai <nguyentritai2906@gmail.com>
2023-01-17 20:57:04 +0700

    feat(startup): instruction on first run

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if Neovim is installed
-is_installed=$(command -v nvim 2>&1 >/dev/null; echo $?) # 0 if installed, 1 if not
+is_installed=$(command -v nvim 2>&1 >/dev/null; echo $?)
 
 # Check if neovim is >= 0.8
 if [[ $is_installed -eq 0 ]]; then
@@ -16,7 +16,6 @@ fi
 if [[ $is_installed -eq 1 ]]; then
     echo "Neovim >= 0.8 is required"
 
-    # echo EOF
     echo "You can install it with:"
     echo "  sudo add-apt-repository ppa:neovim-ppa/unstable"
     echo "  sudo apt update"
@@ -36,5 +35,8 @@ then
     echo "You can install it with:"
     echo "  sudo apt install npm"
 
-    exit
+    exit 1
 fi
+
+echo "Neovim is installed"
+echo "Open Neovim (ignore errors on first run) and run :PackerSync"
