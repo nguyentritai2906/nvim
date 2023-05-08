ad609babc40d26625f9167052a5438abd967a4b4
Nguyen Tri Tai <nguyentritai2906@gmail.com>
2023-05-08 14:58:33 +0700

    chore(indent): configuration

--- after/plugin/indent.lua
+++ after/plugin/indent.lua
@@ -1,3 +1,9 @@
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
-require('indent_blankline').setup {char = '┊', show_trailing_blankline_indent = false}
+require('indent_blankline').setup {
+    -- char = '┊',
+    show_trailing_blankline_indent = false,
+    space_char_blankline = " ",
+    show_current_context = true
+    -- show_current_context_start = true
+}
